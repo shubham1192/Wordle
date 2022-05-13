044a24df54d527c238d564489665eb2e588020ff
shubham1192 <54400888+shubham1192@users.noreply.github.com>
2022-05-13 22:51:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const bodyParse = require("body-parser");
 const mongoose = require("mongoose");
 const read = require("body-parser/lib/read");
-// mongoose.connect('mongodb+srv://admin-shubham:test123@cluster0.a5w6k.mongodb.net/todo', {useNewUrlParser: true});
-mongoose.connect("mongodb://localhost:27017/TT", { useNewUrlParser: true });
-// const date = require(__dirname+"/date.js");
 const _ = require("lodash");
 const { stringify } = require("nodemon/lib/utils");
 const req = require("express/lib/request");
@@ -14,11 +11,27 @@ const { name } = require("ejs");
 const res = require("express/lib/response");
 const { use } = require("express/lib/router");
 const app = express();
-
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+console.log(__dirname);
+app.get("/favicon.ico", function (req, res) {
+  res.status(204);
+  res.end();
+});
+const r1=["hey"];
 app.set("view engine", "ejs"); //using ejs & creating a new dir (views/list.ejs)
 
 app.get("/",function(req,res){
   res.render("grid");
+  console.log(r1);
+})
+app.post("/",function(req,res){
+  const n=req.body.item;
+  console.log(n) 
+
+
+  
+
 })
 app.listen(3000,function(){
   console.log("SERVER UP");
