bb6a1a846712e9ed81e44f22726b3a6c2ec5c07a
Charitra <73011331+Charitra-2001@users.noreply.github.com>
2022-05-17 03:23:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,51 +18,179 @@ app.get("/favicon.ico", function (req, res) {
   res.status(204);
   res.end();
 });
-const r1=["hey"];
+const word=["LIVER","POWER","MODEL","STINK","GRADE","QUIET","BENCH","DEATH","FRESH","ARGUE","MAJOR","PAPER","REPAY","FIRST","SHRUB","PRINT","ANGRY","FRONT","BASIC","SPICY","SPEND","PILOT","DOZEN","CLEAN","GREAT","ALIEN","SMART","RADIO","TIGER","SHINE","DRINK","FAVOR","PANIC","POINT","LIGHT","FRAME","BLACK","CHANT","THORN","SHAME","FOCUS ","MOVIE","PLANT","TRAIN","TIPSY","METAL","MONEY","ALBUM","FIELD","NIGHT"]
+var r1;
+const alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 app.set("view engine", "ejs"); //using ejs & creating a new dir (views/list.ejs)
+var count=0;
+var success=0;
+var found=[];
+const F=[]
+var colors=[];
+var C=[]
+var finalCount=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var SuccessCount=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
+var visit=0;
 app.get("/",function(req,res){
-  res.render("grid");
-  console.log(r1);
+  var aa = Math.floor(Math.random()*50);
+  r1=word[aa];
+  console.log(visit)
+  res.render("start");
 })
+
+app.get("/:visit",function(req,res){
+  // count++
+  
+  var a = req.params['visit'];
+  console.log("This is the a: ",a);
+  res.render("grid",{count:finalCount[a-1],letters:F,Colors:C,
+  success:SuccessCount[a-1],visitor:req.params['visit'],item:found});
+  // console.log(r1);
+  // console.log(F);
+  // console.log(count);
+})
+
 app.post("/",function(req,res){
-  const n=req.body.item;
-  console.log(n) 
+  let c=[];
+  console.log("Current word is: ",r1);
+  const u = req.body.visitor;
+  finalCount[u-1]++;
+  console.log("This is the final count: ",finalCount[u-1]);
+  console.log("This is the Uth ",u);
+  var arr=req.body.item;//! Jo le rhe hai hum
+  var up=arr.map(x=> x.toUpperCase())
+  console.log("This is the indexing ", F[u-1]);
+  if(F[u-1]===undefined)
+  {
+    
+  }
+  else
+  {
+    found=F[u-1];
+    console.log("This is the found array: ",found);
+  }
+  let s="";
+  for(var i=0;i<arr.length;i++)
+  {
+    s+=up[i];
+  }
 
-
+  found.push(up);
   
 
+  if(F[u-1]===undefined)
+  {
+    F.push(found)   
+  }
+  else
+  {
+    F[u-1]=found;
+  }  
+  console.log(count);
+  console.log('This is the F array ',F);
+  //! Step 1(Matching the string)
+  
+  if(s==r1)
+  {
+    //! All green 
+    console.log("Step-1");
+    for(var i=0;i<s.length;i++)
+    {
+      c.push(1);
+    }
+    if(C[u-1]===undefined)
+    {
+
+    }
+    else
+    {
+      colors=C[u-1];
+    }
+    colors.push(c);
+    // 
+    if(C[u-1]===undefined)
+    C.push(colors)
+    else
+    C[u-1]=colors;
+    SuccessCount[u-1]=1;
+  }
+  else 
+  {
+   
+    var a = r1;
+
+      for(var i=0;i<a.length;i++)
+      {
+        if(a[i]===s[i])
+        {
+          c.push(1);
+        
+          s = s.replace(s[i],'/');
+          a = a.replace(a[i],'/');
+          console.log(s,a);
+          continue;
+        }
+        var check=false;
+        for(var j=0;j<s.length;j++)
+        {
+          if(s[i]===a[j])
+          {
+            s = s.replace(s[i],'/');
+            a = a.replace(a[j],'/');
+            console.log(s,a);
+            // Making it yellow
+            c.push(2);
+            check=true;
+            break;
+          }
+        }
+        if(check===false)
+        {
+          // Making it red
+          c.push(3);
+        }
+      }
+    console.log("This is the color array ", c);
+    if(C[u-1]===undefined)
+    {
+
+    }
+    else
+    {
+      colors=C[u-1];
+    }
+    colors.push(c);
+    // 
+    if(C[u-1]===undefined)
+    C.push(colors)
+    else
+    C[u-1]=colors;
+
+    console.log("This is the important color array ",C);
+  }
+console.log(colors)
+  res.redirect("/"+u)
+
+})
+app.post("/begin",function(req,res){
+
+  visit++;
+  // count=0;
+  // success=0;
+ found=[];
+  colors=[];
+  res.redirect("/"+visit)
+})
+app.post("/play",function(req,res){
+  
+  // count=0;
+//  success=0;
+
+ found=[];
+ colors=[];
+  res.redirect("/")
 })
 app.listen(3000,function(){
   console.log("SERVER UP");
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
